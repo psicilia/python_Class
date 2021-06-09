d739f3c133d25d8b7e53b24d5b465c88d0df3404
Pablo Sicilia <psicilia@lcg.unam.mx>
2021-06-08 20:34:05 -0500

    reorder and tarea 9 file created

---
+++ src/tareas/tarea_9_regiones_AT.py
@@ -0,0 +1,54 @@
+"""
+NAME 
+    tarea_9_regiones_AT.py
+
+VERSION
+    1.0
+
+AUTHOR
+    Pablo Sicilia Andrade  <psicilia@gmail.com>
+
+
+DESCRIPTION
+    imprimir las regiones ricas en AT si solo hay ATCG 
+    en caso contrario imprime los caracteres incorrectos 
+
+
+CATEGORY
+
+USAGE
+  
+
+ARGUMENTS
+  -arg    descripcion 
+
+    
+INPUT
+
+OUTPUT
+
+GITHUB
+
+
+"""
+
+import argparse
+from os import error, replace
+
+# Create the parser
+parser = argparse.ArgumentParser(description="descripcion")
+# Argumentos
+parser.add_argument("-i", "--input",
+    #archivo de entrada, 
+    metavar="path/to/file",
+    help="direccion del archivo de entrada",
+    required=False
+    # cambiar en caso de ser necesario 
+)
+parser.add_argument("-o", "--output", nargs='?', const="not output file" ,
+    #archivo de salida, no necesario
+    help="Direccion del archivo de salida /n al no definir el output se mostrara en terminal",
+     required=True
+)
+# Execute the parse_args() method
+args = parser.parse_args()
